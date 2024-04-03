8a26f31086b19a1f64a5d7f80e5bdd32f8fea3e5
YuNook <hongyr1231@naver.com>
2024-04-03 10:49:57 +0900

    1945스트라이커3 스타트

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        float moveX = moveSpeed * Time.deltaTime * Input.GetAxis("Horizontal");
+        float moveY = moveSpeed * Time.deltaTime * Input.GetAxis("Vertical");
+
+        transform.Translate(moveX, moveY, 0);
+
+        Vector3 viewPos = Camera.main.WorldToViewportPoint(transform.position);
+        viewPos.x = Mathf.Clamp01(viewPos.x); //x값을 0이상, 1이하로 제한한다.
+        viewPos.y = Mathf.Clamp01(viewPos.y); //y값을 0이상, 1이하로 제한한다.
+        Vector3 worldPos = Camera.main.ViewportToWorldPoint(viewPos);//다시월드좌표로 변환
+        transform.position = worldPos;
+    }
+}
